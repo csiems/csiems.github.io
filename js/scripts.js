032fc35350b1937b1f436a25dd3a0483e3c44934
Christopher Siems <csiems@gmail.com>
2015-10-19 19:57:57 -0700

    Add Javascript functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
 
-	
+	$("#pingPongForm").submit(function(event) {
 
+		var pingPongNumber = parseInt($("#pingPongInput").val());
 
-	
+		$("#bucket").empty();
+
+		for(var i = 1; i <= pingPongNumber; i++) {
+
+            if (i % 3 === 0 && i % 5 === 0) {
+                $("#bucket").append("ping-pong<br>");
+            } else if (i % 3 === 0) {
+                $("#bucket").append("ping<br>");
+            } else if (i % 5 === 0) {
+                $("#bucket").append("pong<br>");
+            } else {
+                $("#bucket").append(i + "<br>");
+            }
+
+        }
+			
+        event.preventDefault();
+
+	});
+
 });
