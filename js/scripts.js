85eb24d45a4cc7acbdcbe066d66b40b56900a7ea
csiems <csiems@gmail.com>
2015-10-23 11:54:17 -0700

    Refactor scripts.js to improve testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
+//function to return  ping, pong, pingpong, or number
+var pingPongIt = function(number) {
+    if (number % 15 === 0) {
+        return "pingpong";
+    } else if (number % 5 === 0) {
+        return "pong";
+    } else if (number % 3 === 0) {
+        return "ping";
+    } else {
+        return number;
+    }
+}
+
 $(document).ready(function() {
-
-	$("#pingPongForm").submit(function(event) {
-
-		var pingPongNumber = parseInt($("#pingPongInput").val());
-
-		$("#bucket").empty();
-
-		for(var i = 1; i <= pingPongNumber; i++) {
-
-            if (i % 3 === 0 && i % 5 === 0) {
-                $("#bucket").append("<li>pingpong</li>");
-            } else if (i % 3 === 0) {
-                $("#bucket").append("<li>ping</li>");
-            } else if (i % 5 === 0) {
-                $("#bucket").append("<li>pong</li>");
-            } else {
-                $("#bucket").append("<li>" + i + "</li>");
-            }
-
+    //listener for button press
+    $("#pingPongForm").submit(function(event) {
+        //turn input into usable data
+        var pingPongNumber = parseInt($("#pingPongInput").val());
+        //empty the bucket div (so that previous use of app is removed)
+        $("#bucket").empty();
+        //call function for each case 1 to the number entered; append it to bucket div as a list item
+        for(var i = 1; i <= pingPongNumber; i++) {
+            $("#bucket").append("<li>" + pingPongIt(i) + "</li>");
         }
-			
+        //prevent information from submitting   
         event.preventDefault();
 
-	});
+    });
 
 });
