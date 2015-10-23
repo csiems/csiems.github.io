20e5b3d2ead2bf7391e8f3cc310c84b2392995e2
csiems <csiems@gmail.com>
2015-10-23 15:12:25 -0700

    Add spec files and write tests for all inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-//function to return  ping, pong, pingpong, or number
 var pingPongIt = function(number) {
-    if (number % 15 === 0) {
+    if (number < 1) {
+        return;
+    } else if (number % 15 === 0) {
         return "pingpong";
     } else if (number % 5 === 0) {
         return "pong";
@@ -12,17 +13,17 @@ var pingPongIt = function(number) {
 }
 
 $(document).ready(function() {
-    //listener for button press
+
     $("#pingPongForm").submit(function(event) {
-        //turn input into usable data
+
         var pingPongNumber = parseInt($("#pingPongInput").val());
-        //empty the bucket div (so that previous use of app is removed)
+
         $("#bucket").empty();
-        //call function for each case 1 to the number entered; append it to bucket div as a list item
+
         for(var i = 1; i <= pingPongNumber; i++) {
             $("#bucket").append("<li>" + pingPongIt(i) + "</li>");
         }
-        //prevent information from submitting   
+
         event.preventDefault();
 
     });
---
+++ spec/specs.js
@@ -0,0 +1,21 @@
+describe('pingPongIt', function() {
+  it("will return pingpong if the number is divisible by 15", function() {
+    expect(pingPongIt(45)).to.equal('pingpong');
+  });
+
+  it("will return ping if the number is divisible by 3", function() {
+    expect(pingPongIt(9)).to.equal('ping');
+  });
+
+  it("will return pong if the number is divisible by 5", function() {
+    expect(pingPongIt(25)).to.equal('pong');
+  });
+
+  it("will return the number if the number is not divisible by 3 or 5", function() {
+    expect(pingPongIt(7)).to.equal(7);
+  });
+
+  it("will terminate if number is less than 1", function() {
+    expect(pingPongIt(0)).to.equal();
+  });
+});
